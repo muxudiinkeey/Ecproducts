767ede9ecef8560908d3e039895ff43ad8b6e272
Mohiaden Mohamoud <muxudiinkeey@gmail.com>
2025-04-06 12:03:22 +0200

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'path' },
+    { path: 'productList', component: ProductListComponent},
+    { path: 'productDetails', component: ProductdetailsComponent },
+    { path: 'addProduct', component: AddProductComponent},
+    { path: '**', component: PageNotFoundComponent },
+];
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable,inject } from '@angular/core';
+import { product } from './product';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  http = inject (HttpClient)
+
+  getProducts() {
+    return this.http.get<product>(`http://localhost:3000/products`);
+  }
+  getProduct(id: string) {
+    return this.http.get<product>(`http://localhost:3000/products/${id}`);
+  }
+
+  updateProduct(product: product) {
+    return this.http.put<product>(
+      `http://localhost:3000/products/${product.id}`,
+      product
+    );
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete(`http://localhost:3000/products/${id}`);
+  }
+  addProduct(data: product) {
+    return this.http.post('http://localhost:3000/products', data);
+  }
 }
